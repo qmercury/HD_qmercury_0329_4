148f5e2a3f8767b0531f912a8f03dd0a9371ac37
qmercury <qmercury@knou.ac.kr>
2022-03-31 14:43:13 +0900

    유니티스터디 0329 연습문제 4번

---
+++ HD_qmercury_0329_4.cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HD_qmercury_0329_4.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                    
+                }
+                Console.WriteLine("");
+            }
+            
+        }
+    }
+}
+//for만을 사용하여 아래 별을 출력하세요.
+//다음 별모양 출력하기
+//*
+//**
+//***
+//****
+//*****
